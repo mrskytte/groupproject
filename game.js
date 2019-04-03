5304ed7fa1be654b19a750cf2cf1f3c0c0b9a8ef
Christoffer Skytte Wielsøe <Christoffer@wokedaddy-2.local>
2019-04-03 23:08:29 +0200

    Multiple moving astroid added - single line / no randomization of location

--- game.js
+++ game.js
@@ -2,16 +2,15 @@ const startBtn = document.querySelector("#startgame")
 startBtn.addEventListener("click", startGame)
 
 var mySpaceShip;
-var myAstroid;
+var myAstroid = [];
 
 function startGame() {
     //    Create Canvas
     myGameArea.start();
     //    Create Spaceship
     mySpaceShip = new component(30, 10, "yellow", 10, 400);
-    //    Create Astroid
-    myAstroid = new component(50, 50, "red", 1000, 400);
 }
+
 const main = document.querySelector("#main");
 
 //Create Canvas Function
@@ -22,7 +21,10 @@ var myGameArea = {
         this.canvas.height = 800;
         this.context = this.canvas.getContext("2d");
         main.insertBefore(this.canvas, main.childNodes[0]);
+        this.frameNo = 0;
         this.interval = setInterval(updateGameArea, 20);
+
+        // Key Listeners
         window.addEventListener("keydown", function (e) {
             myGameArea.keys = (myGameArea.keys || []);
             myGameArea.keys[e.keyCode] = true;
@@ -77,40 +79,64 @@ function component(width, height, color, x, y) {
 
 //Makes the Game Run (by deleting and adding frames)
 function updateGameArea() {
-    if (mySpaceShip.crashWith(myAstroid)) {
-        myGameArea.stop();
-    } else {
-        myGameArea.clear();
-        myAstroid.update();
-        mySpaceShip.newPos();
-        mySpaceShip.update();
-        mySpaceShip.speedX = 0;
-        mySpaceShip.speedY = 0;
+    var x, y;
 
-
-
-        //Functions that determines movement speed (number) and direction (Y or X)
-
-        //    UP
-        if (myGameArea.keys && myGameArea.keys[38]) {
-            mySpaceShip.speedY -= 2;
+    for (i = 0; i < myAstroid.length; i += 1) {
+        if (mySpaceShip.crashWith(myAstroid[i])) {
+            myGameArea.stop();
+            return;
+            console.log("for number 1")
         }
-
-        //    DOWN
-        if (myGameArea.keys && myGameArea.keys[40]) {
-            mySpaceShip.speedY += 2;
-        }
-
-        //Untag if we need left and right movement*
-
-        //    LEFT
-        if (myGameArea.keys && myGameArea.keys[37]) {
-            mySpaceShip.speedX -= 2;
-        }
-        //    RIGHT
-        if (myGameArea.keys && myGameArea.keys[39]) {
-            mySpaceShip.speedX += 2;
-        }
-
     }
+
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+
+    if (myGameArea.frameNo == 1 || everyInterval(150)) {
+        x = myGameArea.canvas.width;
+        y = myGameArea.canvas.height - 200;
+        myAstroid.push(new component(20, 20, "red", x, y));
+        console.log("component")
+    }
+    for (i = 0; i < myAstroid.length; i += 1) {
+        myAstroid[i].x -= 1;
+        myAstroid[i].update();
+        console.log("got number 2")
+    }
+    mySpaceShip.newPos();
+    mySpaceShip.update();
+    mySpaceShip.speedX = 0;
+    mySpaceShip.speedY = 0;
+
+
+    //Functions that determines movement speed (number) and direction (Y or X)
+
+    //    UP
+    if (myGameArea.keys && myGameArea.keys[38]) {
+        mySpaceShip.speedY -= 2;
+    }
+
+    //    DOWN
+    if (myGameArea.keys && myGameArea.keys[40]) {
+        mySpaceShip.speedY += 2;
+        console.log(myGameArea.frameNo)
+    }
+
+    //Untag if we need left and right movement*
+
+    //    LEFT
+    if (myGameArea.keys && myGameArea.keys[37]) {
+        mySpaceShip.speedX -= 2;
+    }
+    //    RIGHT
+    if (myGameArea.keys && myGameArea.keys[39]) {
+        mySpaceShip.speedX += 2;
+    }
+
+}
+function everyInterval(n) {
+    if ((myGameArea.frameNo / n) % 1 == 0) {
+        return true;
+    }
+    return false;
 }
